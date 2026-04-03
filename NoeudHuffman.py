83acddd6ee23207cc0efb0b1fb6fe92e3b7d93d0
jadee <jade.rakotoharisoa0etu.u-pec.fr>
2026-04-03 09:50:30 +0200

    Ajout de la classe NoeudHuffman avec son constructeur, ses getters et son affichage

--- NoeudHuffman.py
+++ NoeudHuffman.py
@@ -1,68 +1,44 @@
-class NoeudBinaire:
+from NoeudBinaire import NoeudBinaire
 
-    def __init__(self, valeur, gauche=None, droit=None): #constructeur
-        self._valeur = valeur # le '_' signifie que l'attributs est privé
-        self._gauche = gauche
-        self._droit = droit
-
-    #Getters 
-    def get_valeur(self):
-        return self._valeur
+class NoeudHuffman(NoeudBinaire):
+    """
+    Arbre de Huffman : arbre binaire ou chaque nœud
+    a pour valeur un tuple (chaine, poids).
+    - chaine : str = les caractères du sous-arbre
+    - poids  : int = somme des effectifs des caractères
     
-    def get_gauche(self):
-        return self._gauche
-    
-    def get_droit(self):
-        return self._droit
+    Les setters ne sont pas redéfinis ici car :
+    - on ne modifie jamais un nœud existant dans l'algorithme de Huffman
+    - on crée toujours de nouveaux nœuds parents lors des fusions
+    - les setters de NoeudBinaire sont déjà hérités si besoin
+    """
 
-    #Setters
-    def set_valeur(self,nouvelle_valeur):
-        pass
-
-    def set_gauche(self, nouveau_gauche):
-        pass
-
-    def set_droit(self, nouveau_droit):
-        pass
-
-    #Méthodes
-    #Membre A
-    def est_vide(self):
-        pass
-        
-    def est_feuille(self):
-        pass
-         
-        
-    def admet_gauche(self):
-        pass
-
-    def admet_droit(self):      
-        pass
-    
-    def hauteur(self):
-        pass
-    
-    def ajouter_element(self, element):
-        pass
-    
-    #Membre B
-    def parcours_prefixe(self):
-        pass
+    # Constructeur                                                   
+    def __init__(self, chaine, poids, gauche=None, droit=None):
+        # On appelle le constructeur de NoeudBinaire en passant un tuple (chaine, poids) comme valeur du nœud
+        super().__init__((chaine, poids), gauche, droit)
 
 
-    def parcours_infixe(self):
-        pass
+    # Getters                                                              
+    def get_chaine(self):
+        # Retourne la chaine de caractères du nœud (1er élément du tuple)
+        return self._valeur[0]
 
-    
-    def parcours_suffixe(self):
-        pass
+    def get_poids(self):
+        # Retourne le poids du nœud, la somme des effectifs des caractères présents dans ce sous-arbre (2ème élément du tuple)
+        return self._valeur[1]
 
 
-    def parcours_largeur(self):
-        pass
+    # Affichage    
+    def __str__(self, niveau=0):
+        """Affichage hiérarchique de l'arbre avec indentation."""
+        # On affiche le nœud actuel avec une indentation proportionnelle à son niveau
+        res = "    " * niveau + f"|-> {self._valeur}\n"
 
-   
-    def __str__(self):
-        pass
-    
+        # On affiche récursivement le sous-arbre gauche puis le sous-arbre droit
+        if self.admet_gauche():
+            res += self._gauche.__str__(niveau + 1)
+        if self.admet_droit():
+            res += self._droit.__str__(niveau + 1)
+
+        return res
