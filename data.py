0fbbe7458d4c7dfcf68332d1d6217197b8bec4a8
Manoah_Gilles <manoah.gilles@etu.u-pec.fr>
2026-04-17 09:46:16 +0200

    Correction du fichier data.py

--- data.py
+++ data.py
@@ -4,38 +4,13 @@ from unidecode import unidecode
 
 
 def nettoyer_texte(texte):
-    """Convertit le texte en ASCII en supprimant les accents et diacritiques."""
+    """Nettoie le texte en supprimant les accents et diacritiques."""
     return unidecode(texte)
+
 
 def taille_ascii(texte):
     """Retourne la taille du texte encodé en ASCII (en bits) : 8 bits par caractère."""
     return len(texte) * 8
-
-
-def ascii_vers_base2(c):
-    """
-    Reçoit un caractère c et retourne sa représentation binaire
-    sur 8 bits sous forme de chaine de caractères composée de '0' et '1'.
-    Exemple : ascii_vers_base2('a') -> '01100001'
-    """
-    valeur = ord(c)
-    resultat = ""
-    for i in range(7, -1, -1):
-        bit = (valeur >> i) & 1
-        resultat += str(bit)
-    return resultat
-
-
-def texte_vers_base2(texte):
-    """
-    Retourne la représentation binaire complète d'un texte ASCII
-    sous forme de chaine de '0' et '1'.
-    Exemple : texte_vers_base2("ab") -> '0110000101100010'
-    """
-    resultat = ""
-    for c in texte:
-        resultat += ascii_vers_base2(c)
-    return resultat
 
 
 def taille_compressee(texte_compresse):
